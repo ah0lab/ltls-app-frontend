813b7357052a47be902ca5e44a44c8eee5a6a7ed
Brennin Landrean <landreab@sheridancollege.ca>
2021-09-16 23:47:25 -0400

    Created Tips page
    Tips page created

--- src/app/components/view-tips/view-tips.component.ts
+++ src/app/components/view-tips/view-tips.component.ts
@@ -7,8 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewTipsComponent implements OnInit {
 
+  flagApp: boolean;
+  flagParent: boolean;
+  flagHear: boolean;
+
+  tipApp: string[];
+  tipParent: string[];
+  tipHear: string[];
+
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.flagApp = false;
+    this.flagParent = false;
+    this.flagHear = false;
+
+    this.tipApp = ['This is app tip 1', 'this is app tip 2!'];
+    this.tipParent = ['This is a tip for parents using the app!', 'Another tip for the parents :)'];
+    this.tipHear = ['Try moving the phone closer!', 'Try turning up the sound on your device!'];
+  }
+
+  toggleApp(){
+    this.flagApp = !this.flagApp;
+  }
+
+  toggleParent(){
+    this.flagParent = !this.flagParent;
+  }
+
+  toggleHear(){
+    this.flagHear = !this.flagHear;
+  }
 
 }
