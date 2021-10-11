d2173e56e89d4cf0dfcd4e6c60c921880887dbd6
Brennin Landrean <landreab@sheridancollege.ca>
2021-10-10 21:25:28 -0400

    Updated to fit new narrative
    Updated the flow of the playsound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'tutorial',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
 import {IonicModule} from '@ionic/angular';
 import {FormsModule} from '@angular/forms';
 import {LtlsSoundTestComponent} from './ltls-sound-test/ltls-sound-test.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, LtlsSoundTestComponent],
+  declarations: [ LtlsSoundTestComponent],
   imports: [
     CommonModule,
     IonicModule,
     FormsModule,
   ],
   exports: [
-    LoginComponent,
     LtlsSoundTestComponent
   ]
 })
